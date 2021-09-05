5b1d8a994b078ac14cf62ff555873394df72ae42
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-09-05 16:11:41 -0300

    update project info

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject common-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject net.clojars.macielti/common-clj "0.1.0"
+  :description "Just common Clojure code that I use across projects"
 
-  :url "http://example.com/FIXME"
+  :url "https://github.com/macielti/common-clj"
 
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [prismatic/schema "1.1.11"]]
 
-  :repl-options {:init-ns common-clj.core})
+  :repl-options {:init-ns common-clj.schema.core})
