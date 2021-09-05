c88be7904048bc7a58e9ffcb5fca9cccb90928dc
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-09-05 18:26:18 -0300

    fix typo in doc string

--- src/common_clj/schema/core.clj
+++ src/common_clj/schema/core.clj
@@ -2,7 +2,7 @@
   (:require [schema.core :as s]))
 
 (defn loose-schema
-  "Takes a schema and convert is to a loose schema, so we can validate schemas that fulfill
+  "Takes a schema and convert is to a loose schema, so we can validate schemas that fulfils
   definition and accept more properties than defined initially."
   [schema]
   (assoc schema s/Keyword s/Any))
