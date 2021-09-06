48ad6b80fde72415dc1c3c2bdc37746adb1b6f9e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-09-06 11:14:32 -0300

    add lein cloverage

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :plugins [[lein-codox "0.10.7"]]
+  :plugins [[lein-codox "0.10.7"]
+            [lein-cloverage "1.2.2"]]
 
   :codox {:metadata    {:doc "Just common Clojure code that I use across projects."}
           :output-path "docs"}
