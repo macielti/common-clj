27664a0b49d1799769a6ef98d8442adc21fc909c
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-10-23 19:36:32 -0300

    add env support on config component

--- src/common_clj/component/config.clj
+++ src/common_clj/component/config.clj
@@ -3,15 +3,16 @@
             [common-clj.keyword.core :as keyword.core]
             [cheshire.core :as json]))
 
-(defrecord Config [path]
+(defrecord Config [path env]
   component/Lifecycle
   (start [this]
-    (let [config (json/parse-string (slurp path)
-                                    keyword.core/str->keyword-kebab-case)]
+    (let [config (-> (json/parse-string (slurp path)
+                                        keyword.core/str->keyword-kebab-case)
+                     env)]
       (merge this config)))
 
   (stop [this]
     (assoc this :config nil)))
 
-(defn new-config [path]
-  (map->Config {:path path}))
+(defn new-config [path env]
+  (map->Config {:path path :env env}))
