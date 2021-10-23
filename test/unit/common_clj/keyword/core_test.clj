1d141b31f7ffaa3565b06fc5dede4bda33f7a383
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-10-23 16:56:13 -0300

    add some unit tests

---
+++ test/unit/common_clj/keyword/core_test.clj
@@ -0,0 +1,11 @@
+(ns common-clj.keyword.core-test
+  (:require [schema.test :as s]
+            [clojure.test :refer :all]
+            [common-clj.keyword.core :as keyword.core]))
+
+(use-fixtures :once s/validate-schemas)
+
+(s/deftest str->keyword-kebabed-case-test
+  (testing "that loweCamelCase strings can be converted to kebab case"
+    (is (= :datomic-uri
+           (keyword.core/str->keyword-kebab-case "datomicUri")))))
