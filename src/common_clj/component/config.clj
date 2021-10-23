5c81822a90b285c132cbe3d4443cc46eaba718e5
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-10-23 16:50:17 -0300

    config component more generalist

--- src/common_clj/component/config.clj
+++ src/common_clj/component/config.clj
@@ -3,15 +3,15 @@
             [common-clj.keyword.core :as keyword.core]
             [cheshire.core :as json]))
 
-(defrecord Config []
+(defrecord Config [path]
   component/Lifecycle
   (start [this]
-    (let [config (json/parse-string (slurp "resources/config.json")
+    (let [config (json/parse-string (slurp path)
                                     keyword.core/str->keyword-kebab-case)]
       (assoc this :config config)))
 
   (stop [this]
     (assoc this :config nil)))
 
-(defn new-config []
-  (->Config))
+(defn new-config [path]
+  (->Config {:path path}))
