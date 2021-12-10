e8429d7bf81f7be9e276ad41dfc3a93ba116f7a3
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-12-09 22:04:48 -0300

    wip - adding temporary content to config component

--- src/common_clj/component/config.clj
+++ src/common_clj/component/config.clj
@@ -1,7 +1,8 @@
 (ns common-clj.component.config
   (:require [com.stuartsierra.component :as component]
             [common-clj.keyword.core :as keyword.core]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [schema.core :as s]))
 
 (defrecord Config [path env]
   component/Lifecycle
@@ -9,10 +10,15 @@
     (let [config (-> (json/parse-string (slurp path)
                                         keyword.core/str->keyword-kebab-case)
                      env)]
-      (merge component {:config config})))
+      (merge component {:config (assoc config
+                                  :env env
+                                  :path path)})))
 
   (stop [component]
     (assoc component :config nil)))
 
 (defn new-config [path env]
   (map->Config {:path path :env env}))
+
+(s/defn insert!
+  [write-to])
