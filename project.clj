e453768e3567454131958f35e689a102cdc91a2f
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-12-27 13:09:50 -0300

    add dependences

--- project.clj
+++ project.clj
@@ -4,26 +4,27 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :plugins [[lein-codox "0.10.7"]
+  :plugins [[lein-codox "0.10.8"]
             [lein-cloverage "1.2.2"]]
 
   :codox {:metadata    {:doc "Just common Clojure code that I use across projects."}
           :output-path "docs"}
 
   :dependencies [[com.attendify/schema-refined "0.3.0-alpha4"]
-                 [io.pedestal/pedestal.service "0.5.7"]
+                 [telegrambot-lib "1.2.0"]
+                 [io.pedestal/pedestal.service "0.5.9"]
                  [prismatic/schema-generators "0.1.3"]
                  [nubank/matcher-combinators "3.3.1"]
                  [com.datomic/datomic-free "0.9.5697"]
-                 [io.pedestal/pedestal.jetty "0.5.7"]
+                 [io.pedestal/pedestal.jetty "0.5.9"]
                  [io.pedestal/pedestal.route "0.5.7"]
                  [com.stuartsierra/component "1.0.0"]
-                 [org.clojure/tools.logging "1.1.0"]
-                 [org.clojure/clojure "1.10.1"]
-                 [prismatic/plumbing "0.5.5"]
+                 [org.clojure/tools.logging "1.2.3"]
+                 [org.clojure/clojure "1.10.3"]
+                 [prismatic/plumbing "0.6.0"]
                  [camel-snake-kebab "0.4.2"]
-                 [prismatic/schema "1.1.11"]
-                 [cheshire "5.10.0"]]
+                 [prismatic/schema "1.2.0"]
+                 [cheshire "5.10.1"]]
 
   :test-paths ["test/unit" "test/integration" "test/helpers"]
 
---
+++ src/common_clj/component/telegram.clj
@@ -0,0 +1,1 @@
+(ns common-clj.component.telegram)
