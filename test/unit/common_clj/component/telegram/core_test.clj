16aa1aabafd5c218b74cb6c93466ed625664b232
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-12-27 16:31:47 -0300

    add more tests

--- test/unit/common_clj/component/telegram/core_test.clj
+++ test/unit/common_clj/component/telegram/core_test.clj
@@ -8,8 +8,13 @@
 
 (def test-state (atom nil))
 
+(def chat-id 123456789)
 (def token "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11")
 (def telegram (telegram-bot/create token))
+(def components
+  {:telegram telegram
+   :config   {:telegram {:token   token
+                         :chat-id chat-id}}})
 (def update {:update_id 123456789
              :message   {:chat {:id 123456789}
                          :text "/test testing"}})
@@ -31,12 +36,12 @@
 
 (s/deftest consume-update!-test
   (testing "that we can consume a update"
-    (component.telegram.core/consume-update! update consumers {:telegram {:token token}})
+    (component.telegram.core/consume-update! update consumers components)
     (is (= "/test testing"
            @test-state))
     (reset! test-state nil))
   (testing "that we can handle exception with error-handler provided by the user of the component"
-    (component.telegram.core/consume-update! update-with-exception-in-command-consumption consumers {:telegram {:token token}})
+    (component.telegram.core/consume-update! update-with-exception-in-command-consumption consumers components)
     (is (= {:cause :nothing}
            @test-state))
     (reset! test-state nil))
@@ -47,7 +52,19 @@
                                                                                             (json/parse-string true)
                                                                                             :text))
                                                                      {})}
-      (component.telegram.core/consume-update! update-with-unmatched-command-message consumers {:telegram telegram}))
+      (component.telegram.core/consume-update! update-with-unmatched-command-message consumers components))
     (is (= "Sorry, command not found.\n"
            @test-state))
     (reset! test-state nil)))
+
+(deftest send-message!-test
+  (fake/with-fake-routes
+    {(format "https://api.telegram.org/bot%s/sendMessage" token) (fn [{:keys [body] :as request}]
+                                                                   (reset! test-state (-> (slurp body)
+                                                                                          (json/parse-string true)
+                                                                                          :text))
+                                                                   {})}
+    (component.telegram.core/send-message! "Random message" components))
+  (is (= "Random message"
+         @test-state))
+  (reset! test-state nil))
