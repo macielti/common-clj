e4f1858fac306b347ae6d27809ebd714525f4b90
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-08-13 19:48:15 -0300

    add todo items so we don't forget to add unit and integration tests

--- src/common_clj/traceability/core.clj
+++ src/common_clj/traceability/core.clj
@@ -3,16 +3,19 @@
 
 (def ^:dynamic *correlation-id* nil)
 
+;TODO: Add missing unit test for that fn
 (s/defn current-correlation-id-from-request-context :- (s/maybe s/Str)
   [request-context]
   (-> (:headers request-context)
       (get-in "x-correlation-id" "DEFAULT")))
 
+;TODO: Add missing unit test for that fn
 (s/defn correlation-id-appended :- s/Str
   [correlation-id]
   (-> (str correlation-id "." (random-uuid))
       clojure.string/upper-case))
 
+;TODO: Add missing integration test for that fn
 (s/defn with-correlation-id
   [http-request-handler-fn]
   (s/fn [request-context]
