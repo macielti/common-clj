d51ba62b40e755ad1ddafd828281999556b0d49e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-09-17 13:41:20 -0300

    add success integration test case

--- test/integration/integration/recaptcha_validation_interceptor_test.clj
+++ test/integration/integration/recaptcha_validation_interceptor_test.clj
@@ -7,7 +7,9 @@
             [schema.test :as schema-test]
             [common-clj.component.helper.core :as component.helper]
             [integration.aux.http :as aux.http]
-            [common-clj.io.interceptors.auth :as io.interceptors.auth]))
+            [common-clj.io.interceptors.auth :as io.interceptors.auth]
+            [clj-http.fake :as http.fake]
+            [cheshire.core :as json]))
 
 ;TODO: Check if we called the right external endpoints passing the right params (need a http component that maintain a history of requests that were made during test)
 
@@ -37,21 +39,32 @@
     :routes (component/using (component.routes/new-routes routes-example) [:config])
     :service (component/using (component.service/new-service) [:config :routes])))
 
-(schema-test/deftest recaptcha-validation-interceptor-enabled-test
-  (let [system (component/start system-test-with-recaptcha-validation-enabled)
-        service-fn (-> (component.helper/get-component-content :service system)
-                       :io.pedestal.http/service-fn)]
-    (testing "we can't request the endpoint without passing the 'X-Recaptcha-Token' header if the recaptcha-validation is enabled"
-      (is (= {:status 400
-              :body   {:error   "not-able-to-perform-recaptcha-validation"
-                       :message "Not able to check the success completion of the reCAPTCHA challenge"
-                       :detail  {:error "not-able-to-perform-recaptcha-validation"}}}
-             (aux.http/request-test-endpoints "/recaptcha-validation-interceptor-test" {} service-fn))))
+(def valid-recaptcha-token-response "valid-recaptcha-token-response")
 
-    (testing "we can't request the endpoint passing the 'X-Recaptcha-Token' header with a not valid response token"
-      (is (= {:status 400
-              :body   {:error   "not-able-to-perform-recaptcha-validation"
-                       :message "Not able to check the success completion of the reCAPTCHA challenge"
-                       :detail  {:error "not-able-to-perform-recaptcha-validation"}}}
-             (aux.http/request-test-endpoints "/recaptcha-validation-interceptor-test" {"X-Recaptcha-Token" "random-stuff"} service-fn))))
-    (component/stop-system system)))
+(schema-test/deftest recaptcha-validation-interceptor-enabled-test
+  (http.fake/with-fake-routes
+    {"https://www.google.com/recaptcha/api/siteverify?secret=ednaldo-pereira&response=valid-recaptcha-token-response" {:post (constantly {:status 200
+                                                                                                                                          :body   (json/encode {:success true
+                                                                                                                                                                :score   0.8})})}}
+    (let [system (component/start system-test-with-recaptcha-validation-enabled)
+          service-fn (-> (component.helper/get-component-content :service system)
+                         :io.pedestal.http/service-fn)]
+      (testing "we can't request the endpoint without passing the 'X-Recaptcha-Token' header if the recaptcha-validation is enabled"
+        (is (= {:status 400
+                :body   {:error   "not-able-to-perform-recaptcha-validation"
+                         :message "Not able to check the success completion of the reCAPTCHA challenge"
+                         :detail  {:error "not-able-to-perform-recaptcha-validation"}}}
+               (aux.http/request-test-endpoints "/recaptcha-validation-interceptor-test" {} service-fn))))
+
+      (testing "we can't request the endpoint passing the 'X-Recaptcha-Token' header with a not valid response token"
+        (is (= {:status 400
+                :body   {:error   "not-able-to-perform-recaptcha-validation"
+                         :message "Not able to check the success completion of the reCAPTCHA challenge"
+                         :detail  {:error "not-able-to-perform-recaptcha-validation"}}}
+               (aux.http/request-test-endpoints "/recaptcha-validation-interceptor-test" {"X-Recaptcha-Token" "random-stuff"} service-fn))))
+
+      (testing "we can request the endpoint passing the 'X-Recaptcha-Token' header with a valid response token"
+        (is (= {:status 200
+                :body   nil}
+               (aux.http/request-test-endpoints "/recaptcha-validation-interceptor-test" {"X-Recaptcha-Token" valid-recaptcha-token-response} service-fn))))
+      (component/stop-system system))))
