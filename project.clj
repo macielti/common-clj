af929d22fa0b7d922ea57c7f98ff2066144f0c2c
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-11-13 10:08:09 -0300

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -31,7 +31,7 @@
                  [io.pedestal/pedestal.service "0.5.10"]
                  [io.pedestal/pedestal.interceptor "0.5.10"]
                  [prismatic/schema-generators "0.1.4"]
-                 [nubank/matcher-combinators "3.5.1"]
+                 [nubank/matcher-combinators "3.6.0"]
                  [com.datomic/datomic-free "0.9.5697"]
                  [io.pedestal/pedestal.jetty "0.5.10"]
                  [io.pedestal/pedestal.route "0.5.10"]
