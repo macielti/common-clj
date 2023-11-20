315522889a26a98145aeb1bab1be261bba03d7fe
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-20 12:07:26 -0300

    lint

--- src/common_clj/io/interceptors.clj
+++ src/common_clj/io/interceptors.clj
@@ -1,13 +1,12 @@
 (ns common-clj.io.interceptors
-  (:require
-    [common-clj.error.core :as common-error]
-    [humanize.schema :as h]
-    [io.pedestal.http :as http]
-    [io.pedestal.http.body-params :as body-params]
-    [io.pedestal.interceptor :as pedestal.interceptor]
-    [io.pedestal.interceptor.error :as error]
-    [schema.core :as s]
-    [taoensso.timbre :as log])
+  (:require [common-clj.error.core :as common-error]
+            [humanize.schema :as h]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.interceptor :as pedestal.interceptor]
+            [io.pedestal.interceptor.error :as error]
+            [schema.core :as s]
+            [taoensso.timbre :as log])
   (:import (clojure.lang ExceptionInfo)))
 
 (def error-handler-interceptor
