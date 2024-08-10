d5c74ffb21aa159ef5a8c083172cb04863f37915
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-08-10 13:38:20 -0300

    fix tests

--- test/integration/integration/new_relic_component_test.clj
+++ test/integration/integration/new_relic_component_test.clj
@@ -5,6 +5,7 @@
             [common-clj.component.helper.core :as component.helper]
             [common-clj.component.http-client :as component.http-client]
             [common-clj.component.new-relic :as component.new-relic]
+            [matcher-combinators.test :refer [match?]]
             [schema.test :as s]
             [taoensso.timbre :as log]))
 
@@ -23,15 +24,15 @@
     (Thread/sleep 1000)
 
     (testing "that we can fetch the requests made by the http-client sent to new relic"
-      (is (= [{:method  :post
-               :payload {:body    {:hostname  "bruno.nascimento"
-                                   :level     "warn"
-                                   :message   ":testing"
-                                   :namespace "integration.new-relic-component-test"
-                                   :service   "test-service-name"}
-                         :headers {"Api-Key"      "random-api-key"
-                                   "Content-Type" "application/json"}}
-               :url     "https://log-api.newrelic.com/log/v1"}]
-             (component.http-client/requests http-client))))
+      (is (match? [{:method  :post
+                    :payload {:body    {:hostname  string?
+                                        :level     "warn"
+                                        :message   ":testing"
+                                        :namespace "integration.new-relic-component-test"
+                                        :service   "test-service-name"}
+                              :headers {"Api-Key"      "random-api-key"
+                                        "Content-Type" "application/json"}}
+                    :url     "https://log-api.newrelic.com/log/v1"}]
+                  (component.http-client/requests http-client))))
 
     (component/stop system)))
