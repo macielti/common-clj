aa110737d962e85c4a24cd40ff38fa46ac3f0a7f
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-09-09 13:42:55 -0300

    minor changes

--- project.clj
+++ project.clj
@@ -4,13 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :plugins [[lein-codox "0.10.8"]
-            [lein-cloverage "1.2.4"]
+  :plugins [[lein-cloverage "1.2.4"]
             [com.github.clojure-lsp/lein-clojure-lsp "1.4.9"]
             [com.github.liquidz/antq "RELEASE"]]
-
-  :codox {:metadata    {:doc "Just common Clojure code that I use across projects."}
-          :output-path "docs"}
 
   :exclusions [log4j]
 
