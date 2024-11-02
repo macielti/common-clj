366aa5c77aa600d169641267fc6f483e381acd22
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-02 10:17:37 -0300

    fix conguration - x

--- test/integration/integration/telegram_producer_component_test.clj
+++ test/integration/integration/telegram_producer_component_test.clj
@@ -8,7 +8,7 @@
 
 (def system-test
   (component/system-map
-   :config (component.config/new-config "resources/config_test.json" :test :json)
+   :config (component.config/new-config "test/resources/config_test.json" :test :json)
    :telegram-producer (component/using (component.telegram.producer/new-telegram-producer) [:config])))
 
 (s/deftest telegram-consumer-component-test
