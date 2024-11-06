2c30c4e929174483c24f6819a06c00a283372b9e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-05 22:20:11 -0300

    fix tests

--- test/helpers/fixtures/config.clj
+++
@@ -1,8 +0,0 @@
-(ns fixtures.config
-  (:require [fixtures.update]))
-
-(def token "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11")
-
-(def config {:telegram {:token                token
-                        :chat-id              fixtures.update/chat-id
-                        :message-template-dir "templates"}})
