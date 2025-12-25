a39452a5fbb6c5fe95acccb91d3421a8afbd6613
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2025-12-25 13:29:46 -0300

    update dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clojars.macielti/common-clj "43.74.74"
+(defproject net.clojars.macielti/common-clj "43.74.75"
 
   :description "Just common Clojure code that I use across projects"
 
@@ -9,37 +9,37 @@
 
   :exclusions [log4j]
 
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.4"]
                  [prismatic/schema-generators "0.1.5"]
                  [siili/humanize "0.1.1"]
                  [camel-snake-kebab "0.4.3"]
                  [prismatic/schema "1.4.1"]
                  [buddy/buddy-sign "3.6.1-359"]
-                 [cheshire "5.13.0"]
+                 [cheshire "6.1.0"]
                  [metosin/schema-tools "0.13.1"]
                  [clojure.java-time "1.4.3"]
                  [clj-rate-limiter "0.1.6-RC1"]
-                 [dev.weavejester/medley "1.8.1"]
+                 [dev.weavejester/medley "1.9.0"]
                  [hara/io.scheduler "3.0.12"]
-                 [integrant "0.13.1"]
-                 [diehard "0.11.12"]
+                 [integrant "1.0.1"]
+                 [diehard "0.12.0"]
                  [org.clojure/tools.logging "1.3.0"]
                  [danlentz/clj-uuid "0.2.0"]
-                 [commons-io/commons-io "2.18.0"]]
+                 [commons-io/commons-io "2.21.0"]]
 
   :profiles {:dev {:resource-paths ^:replace ["test/resources"]
 
                    :test-paths     ^:replace ["test/unit" "test/integration" "test/helpers"]
 
                    :plugins        [[lein-cloverage "1.2.4"]
-                                    [com.github.clojure-lsp/lein-clojure-lsp "1.4.16"]
+                                    [com.github.clojure-lsp/lein-clojure-lsp "2.0.13"]
                                     [com.github.liquidz/antq "RELEASE"]]
 
-                   :dependencies   [[net.clojars.macielti/common-test-clj "5.2.3"]
-                                    [org.slf4j/slf4j-api "2.0.16"]
-                                    [ch.qos.logback/logback-classic "1.5.16"]
-                                    [net.clojars.macielti/service-component "2.4.2"]
-                                    [nubank/matcher-combinators "3.9.1"]
+                   :dependencies   [[net.clojars.macielti/common-test-clj "5.2.4 "]
+                                    [org.slf4j/slf4j-api "2.0.17"]
+                                    [ch.qos.logback/logback-classic "1.5.23"]
+                                    [net.clojars.macielti/service-component "5.4.2"]
+                                    [nubank/matcher-combinators "3.9.2"]
                                     [clj-http-fake "1.0.4"]
                                     [nubank/mockfn "0.7.0"]
                                     [hashp "0.2.2"]]
